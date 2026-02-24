0e145370f5fd736829d60fa1beaa2b8ba8c84fb3
zodica24 <zodica24@gmail.com>
2026-02-24 15:58:34 -0700

    Update TicketPriceFinder.java
    added a way to throw an error if what is entered is not a valid letter

--- EclipseWorkSpace/CS1050M02/TicketPriceFinder.java
+++ EclipseWorkSpace/CS1050M02/TicketPriceFinder.java
@@ -2,76 +2,73 @@
  * 
  */
 
-/**
- * 
- */
-
 import java.util.Scanner;
 
 
 public class TicketPriceFinder {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		char sectionNumber = ' ';
 		
-		double rowNumber = 0;
+		int rowNumber = 0;
 		
 		int seatPrice = 0;
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("plese enter what section you would like to sit in: F-(Front), C-(Center), S-(Side), U-(Upper) \n");
+		System.out.print("plese enter what section you would like to sit in: F-(Front), C-(Center), S-(Side), U-(Upper).\n");
 		
-		sectionNumber =(char) Character.toUpperCase(input.next().charAt(0));
+		sectionNumber = (char) Character.toUpperCase(input.next().charAt(0));
 		
-		switch(sectionNumber)
+		if (sectionNumber == 'F' || sectionNumber == 'C' || sectionNumber == 'S' ||sectionNumber == 'U' )
 		{
-			case 'F':
-				System.out.print("you chose the Front section.\n");
-				
-				break;
-			case 'C':
-				System.out.print("you chose the Center section.");
-				break;
-			case 'S':
-				System.out.print("you chose the Side section.");
-				break;
-			case 'U':
-				System.out.print("you chose the Upper section.");
-				break;
-			default: 
+			switch(sectionNumber)
+			{
+				case 'F':
+					System.out.print("you chose the Front section.\n");
+					break;
+				case 'C':
+					System.out.print("you chose the Center section.\n");
+					break;
+				case 'S':
+					System.out.print("you chose the Side section.\n");
+					break;
+				case 'U':
+					System.out.print("you chose the Upper section.\n");
+					break;
+				default: 
+					
+					break;
+			}
+			
+			System.out.print("please enter you disired row: 1-60.\n");
+			rowNumber = input.nextInt();
+			
+			if (rowNumber >= 1 && rowNumber <= 15)
+			{
+				seatPrice = 450;
+				System.out.print("Section " + sectionNumber + " row " + rowNumber + " costs $" + seatPrice + ".");
+			}
+			else if (rowNumber >= 16 && rowNumber <= 30)
+			{
+				seatPrice = 300;
+				System.out.print("Section " + sectionNumber + " row " + rowNumber + " costs $" + seatPrice + ".");
+			}
+			else if (rowNumber >= 1 && rowNumber <= 15)
+			{
+				seatPrice = 200;
+				System.out.print("Section " + sectionNumber + " row " + rowNumber + " costs $" + seatPrice + ".");
+			}
+			else
+			{
+				System.out.print("Error: Row entered dos not exist: exiting program.");
+			}
+		}
+			else	
+			{
 				System.out.print("Error: Section choice entered is InValid; closing program.");
-				break;
-		}
-		
-		System.out.print(" please enter you disired row: 1-60.\n");
-		rowNumber = input.next().charAt(0);
-		
-		if (rowNumber >= 1 && rowNumber <= 15)
-		
-		{
-			seatPrice = 450;
-			System.out.print("Section " + sectionNumber + " row " + rowNumber + " costs " + seatPrice + ".");
-		}
-		else if (rowNumber >= 16 && rowNumber <= 30)
-		{
-			seatPrice = 300;
-			System.out.print("Section " + sectionNumber + " row " + rowNumber + " costs " + seatPrice + ".");
-		}
-		else if (rowNumber >= 1 && rowNumber <= 15)
-		{
-			seatPrice = 200;
-			System.out.print("Section " + sectionNumber + " row " + rowNumber + " costs " + seatPrice + ".");
-		}
-		else
-		{
-			System.out.print("Error: Row entered dos not exist: exiting program.");
-		}
+			}
 		
 		input.close();
 	}
