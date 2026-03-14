0264cdf338db27d8e3f6b8ad8d432a7f89f7eed7
zodica24 <zodica24@gmail.com>
2026-03-14 13:52:08 -0600

    Update FitnessTracker.java

--- EclipseWorkSpace/TrainerProject/FitnessTracker.java
+++ EclipseWorkSpace/TrainerProject/FitnessTracker.java
@@ -12,7 +12,14 @@ public class FitnessTracker {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		printSummery();
+		
+		
 	}
 
+	public static void printSummery()
+	{
+		System.out.print("your athleates bmi is and thay are under waight over waight.");
+	}
 }
