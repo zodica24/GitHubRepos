fdc74d081beb4335170b2957ecc1198ccd7db28e
zodica24 <zodica24@gmail.com>
2026-03-14 18:07:48 -0600

    updated fitness tracker

--- EclipseWorkSpace/CS1050MO4/LabL15ArraysIntro.java
+++ EclipseWorkSpace/CS1050MO4/LabL15ArraysIntro.java
@@ -4,7 +4,9 @@
 
 /**
  * 
- */
+ */ 
+import java.util.Scanner;
+
 public class LabL15ArraysIntro {
 
 	/**
@@ -12,7 +14,20 @@ public class LabL15ArraysIntro {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		double[] grades;
+		String[] names;
+		
+		System.out.print("how meany students are in class");
+		int numberOfStudents = input.nextInt();
+		
+		names = new String[numberOfStudents];
+		grades = new double[numberOfStudents];
+		
+		
+		
+		
+		
 	}
 
 }
--- EclipseWorkSpace/TrainerProject/FitnessTracker.java
+++ EclipseWorkSpace/TrainerProject/FitnessTracker.java
@@ -5,6 +5,9 @@
 /**
  * 
  */
+import java.util.Scanner;
+
+
 public class FitnessTracker {
 
 	/**
@@ -13,13 +16,58 @@ public class FitnessTracker {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		printSummery();
-		
-		
+		Scanner input = new Scanner(System.in);
+		//System.out.print(calulateBMI(input));
+		printSummery(input);
 	}
 
-	public static void printSummery()
+	public static void printSummery(Scanner input)
 	{
-		System.out.print("your athleates bmi is and thay are under waight over waight.");
+		System.out.println("your athleates" + calulateBMI(input) + "is and thay are under waight over waight.");
 	}
+	
+	public static double getPositiveDouble(Scanner input, String questionPrompt)
+	{
+		double number = 0;
+		boolean track = true;
+		do {
+			System.out.println(questionPrompt);
+			number = input.nextDouble();
+			if(number > 0)
+			{
+				System.out.println("Number is valid"); 
+				track = false;
+			}
+			else
+			{
+				System.out.println("Number is invalid"); 
+				track = true;
+			}
+		}
+		while(track);
+		
+		return number;
+	}
+	
+	public static double calulateBMI(Scanner input) 
+	{
+		double bmi = 0;
+		double height = 0;
+		double weight = 0;
+		final int bmiNumber = 703;
+		
+				System.out.println("Enter your weight, in pounds: ");
+				
+				weight = getPositiveDouble(input,"plese input a postive number");;
+				
+				System.out.println("Enter your height, in inches: ");
+				
+				height = getPositiveDouble(input,"plese input a postive number");;
+				
+				bmi = weight * bmiNumber / (height * height);
+		
+		return bmi;
+	}
+	
 }
+
