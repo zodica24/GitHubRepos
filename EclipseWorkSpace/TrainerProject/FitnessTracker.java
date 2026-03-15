6b36ff3e1b5d107bdd9062abc5a6b74a86e0f809
zodica24 <zodica24@gmail.com>
2026-03-14 22:08:59 -0600

    Update FitnessTracker.java

--- EclipseWorkSpace/TrainerProject/FitnessTracker.java
+++ EclipseWorkSpace/TrainerProject/FitnessTracker.java
@@ -10,20 +10,19 @@ import java.util.Scanner;
 
 public class FitnessTracker {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+{
 		// TODO Auto-generated method stub
 		
 		Scanner input = new Scanner(System.in);
 		//System.out.print(calulateBMI(input));
+		
 		printSummery(input);
 	}
 
 	public static void printSummery(Scanner input)
 	{
-		System.out.println("your athleates" + calulateBMI(input) + "is and thay are under waight over waight.");
+		System.out.println("your athleates Bmi is" + calulateBMI(input) + " and" + getBMICategory(calulateBMI(input)));
 	}
 	
 	public static double getPositiveDouble(Scanner input, String questionPrompt)
@@ -56,18 +55,73 @@ public class FitnessTracker {
 		double weight = 0;
 		final int bmiNumber = 703;
 		
-				System.out.println("Enter your weight, in pounds: ");
+		System.out.println("Enter your weight, in pounds: ");
 				
-				weight = getPositiveDouble(input,"plese input a postive number");;
+		weight = getPositiveDouble(input,"plese input a postive number");;
 				
-				System.out.println("Enter your height, in inches: ");
+		System.out.println("Enter your height, in inches: ");
 				
-				height = getPositiveDouble(input,"plese input a postive number");;
+		height = getPositiveDouble(input,"plese input a postive number");;
 				
-				bmi = weight * bmiNumber / (height * height);
+		bmi = weight * bmiNumber / (height * height);
 		
 		return bmi;
 	}
 	
+	public static String getBMICategory(double bmi)
+	{
+		String category ="";
+		
+		if (bmi < 18.5)
+		{
+			category = "you are underwaight.";
+		}
+		else if (bmi >= 18.5 && bmi < 25) 
+		{
+			category = "your waight is normal.";
+		}
+		else if (bmi >= 25 && bmi < 30) 
+		{
+			category = "you are overwaight.";
+		}
+		else
+		{
+			category = "you are obese.";
+		}
+		
+		return category;
+	}
+	
+	public static boolean askToContinue(Scanner input)
+	{
+		boolean answer = true;
+		boolean track = true;
+		char chosse =' ';
+		do	
+		{
+			System.out.println("would you like to enter anuther athleate: enter Y/N.");
+			chosse = input.next().charAt(0);
+			if (chosse == 'Y' || chosse == 'N')
+			{
+				if (chosse == 'Y')
+				{
+					answer = true;
+					
+				}
+				else
+				{
+					answer = false;
+				}
+				track = true;
+			}
+			else
+			{
+				track = false;
+			}
+		}
+		while(!track);
+		
+		return answer;
+	}
 }
 
