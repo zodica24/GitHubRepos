be6eb8dc0110c9d32ecc638e5b786751b8b55db9
zodica24 <zodica24@gmail.com>
2026-05-05 11:50:02 -0600

    made new project

---
+++ EclipseWorkSpace/TrainerProject/Project02Trainer.java
@@ -0,0 +1,102 @@
+
+import java.io.FileNotFoundException;
+
+/*
+ * 
+ */
+
+public class Project02Trainer
+{
+	public static void main(String[] args)
+	{
+		displayProgramSummary();
+
+		// ===== TEST 1 =====
+		String fileName = "team1.txt";
+
+		try
+		{
+			System.out.println("\nTesting file: " + fileName);
+
+			Team team = new Team("Nuggets", 6);
+
+			teamSetUp(fileName, team);
+
+			runAnalysis(team);
+
+		} catch (FileNotFoundException exception)
+		{
+			System.out.println("Error: Unable to find file " + fileName);
+		}
+
+		// ===== TEST 2 =====
+		fileName = "team2.txt";
+
+		try
+		{
+			System.out.println("\nTesting file: " + fileName);
+
+			Team team = new Team("Team CS", 4);
+
+			teamSetUp(fileName, team);
+
+			runAnalysis(team);
+
+		} catch (FileNotFoundException exception)
+		{
+			System.out.println("Error: Unable to find file " + fileName);
+		}
+
+		System.out.println("\nEnd of program");
+	}
+
+	public static void teamSetUp(String fileName, Team team) throws FileNotFoundException
+	{
+		// fill in reading information from a file
+	}
+
+	public static void runAnalysis(Team team) throws FileNotFoundException
+	{
+		System.out.println("\n========== Team Analysis ==========");
+		System.out.println("Team: " + team.getTeamName());
+		System.out.println("Total Athletes: " + team.getAthleteCount());
+		System.out.println();
+
+		team.displayAthleteResults();
+		team.displayAthletesOutsideNormalBMI();
+
+		double avg = team.calculateAverageMaxHeartRate();
+		System.out.println("\nAverage Max Heart Rate: " + avg);
+
+		team.displayAthletesAboveAverageMHR(avg);
+		team.displayHighestMHR();
+		team.displaySmallestLargestHeight();
+		String outputFileName = team.getTeamName() + ".txt";
+		team.writeAthletesToFile(outputFileName);
+
+	}
+
+	// ================= DISPLAY =================
+
+	public static void displayProgramSummary()
+	{
+		System.out.println("**************************************");
+		System.out.println("Team and Athlete Analysis");
+		System.out.println("**************************************");
+		System.out.println("Reads athlete data from file");
+		System.out.println("Displays athlete results");
+		System.out.println("Performs team analysis");
+		System.out.println("Writes results to file");
+		System.out.println();
+	}
+}
+
+// ================= ADD ATHLETE CLASS =================
+
+// ================= ADD TEAM CLASS =================
+
+class Team{
+	
+	
+}
+
