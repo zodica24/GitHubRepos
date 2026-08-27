72355041f4b3dabb264babc0332a358ca9d3ddcd
zodica24 <zodica24@gmail.com>
2026-08-27 09:50:11 -0600

    Create ArrayListTesting.java

---
+++ EclipseWorkSpace/firstproject/ArrayListTesting.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ArrayList <String> list = new ArrayList<>();
+		
+		Scanner input = new Scanner(System.in);
+		
+		String myTest = input.nextLine();
+		
+		System.out.print(myTest);
+		
+	}
+
+}
